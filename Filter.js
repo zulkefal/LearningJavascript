a0baca9319c8415c60d8b26ea2fd269b437840a5
zulkefal <zulkefal.khan705@gmail.com>
2024-08-07 10:30:09 +0500

    commit

---
+++ Filter.js
@@ -0,0 +1,13 @@
+const ages = [32, 33, 16, 40];
+// const result = ages.filter(checkAdult);
+
+function checkAdult(age) {
+  return age >= 18;
+}
+
+
+const result = ages.filter(age => {
+    return age >= 18
+})
+
+console.log(result)
---
+++ Slice_Splice.js
@@ -0,0 +1,6 @@
+const cities = ["Tokyo","Cairo","Los Angeles","Paris","Seattle"];
+
+const newCityArr = cities.splice(1,3);
+
+console.log("Original: ", cities)
+console.log("New: ", newCityArr)
